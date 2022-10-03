24d38de13df7612c6174972f69a8df113a1846bd
Diego Lucena <diego_lucena01@live.com>
2022-10-03 16:32:36 -0400

    Add our dry run id

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from remote_donation.models.DonationStateMachine import DonationStateMachine
 
-
-donation_state_machine = DonationStateMachine()
+donation_state_machine = DonationStateMachine(4)
 
 donation_state_machine.run()
