2ea5462e8066c5cac6b2d61f52bdfd7ab65143f5
Diego Lucena <diego_lucena01@live.com>
2022-10-03 13:15:44 -0400

    added chore

--- remote_donation/models/enums/Classes.py
+++ remote_donation/models/enums/Classes.py
@@ -1,6 +1,8 @@
 from enum import Enum
 # https://stackoverflow.com/questions/33690064/dynamically-create-an-enum-with-custom-values-in-python
 # for Dinamic Enums
+
+# TODO: Enum with name + det id. Maybe a class
 class Classes(Enum):
     NONE = "NONE"
     ARROZ = 0
